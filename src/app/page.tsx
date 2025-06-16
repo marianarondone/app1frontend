d3cf3b9b0817f9155caa1c84a7d58151c1d35d21
marianarondone <marianarondone@gmail.com>
2025-06-16 12:38:09 -0400

    Manejo de Estados 16-06

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,41 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useState } from "react";
 
+interface Persona{
+  nombre : string,
+  apellido : string
+}
+let initialStatePersona:Persona = {
+  nombre: "",
+  apellido : ""
+}
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [persona, setPersona] = useState(initialStatePersona)
+  const handlePersona = (name:string, value:string)=>{
+    console.log(name)
+    console.log(value)
+    // validaciones
+    setPersona(
+      {...persona, [name] : value}
+    )
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  }
+  return (
+    <form>
+      <h1>{persona.nombre} {persona.apellido}</h1>
+      <label>Nombre: </label><br/>
+      <input type="text"
+            name = "nombre"
+            placeholder="Nombre"
+            onChange={(e)=>handlePersona(e.currentTarget.name, e.currentTarget.value)}/><br/>
+      <span></span><br/>
+      <label>Apellido: </label><br/>
+      <input type="text" 
+            name = "apellido"
+            placeholder="Apellido"
+            onChange={(e)=>handlePersona(e.currentTarget.name, e.currentTarget.value)}/><br/>
+      <span></span><br/>
+      <button>Registrar</button>
+    </form>
   );
 }
