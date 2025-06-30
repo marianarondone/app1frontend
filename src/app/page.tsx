e696898976c8b60c324414599bba8c85f6607c94
marianarondone <marianarondone@gmail.com>
2025-06-30 11:26:19 -0400

    Update Clase Perdida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,60 @@
 "use client"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Persona{
   nombre : string,
   apellido : string
 }
+
 let initialStatePersona:Persona = {
-  nombre: "",
+  nombre : "",
   apellido : ""
 }
-export default function Home() {
-  const [persona, setPersona] = useState(initialStatePersona)
-  const handlePersona = (name:string, value:string)=>{
-    console.log(name)
-    console.log(value)
-    // validaciones
-    setPersona(
-      {...persona, [name] : value}
-    )
 
+export default function Home() {
+const miStorage = window.localStorage;
+const [Persona, setPersona] = useState(initialStatePersona)
+const [personas, setPersonas] = useState(useState<Persona[]>([]))
+useEffect(() => {
+  console.log("Hola")
+  let listadoSTR = miStorage.getItem("personas")
+  if(listadoSTR != null){
+    let listado = JSON.parse(listadoSTR)
+    setPersonas(listado)
+    console.log(personas)
   }
+ 
+  }, [])
+
+const handlePersona = (name:string, value:string)=>{
+  console.log(name)
+  console.log(value)
+  //validaciones
+  setPersona(
+    {...Persona, [name] : value}
+  )
+};
+const handleRegistrar = ()=>{
+  miStorage.setItem("personas", JSON.stringify([...personas, Persona]))
+}
   return (
-    <form>
-      <h1>{persona.nombre} {persona.apellido}</h1>
-      <label>Nombre: </label><br/>
-      <input type="text"
-            name = "nombre"
-            placeholder="Nombre"
-            onChange={(e)=>handlePersona(e.currentTarget.name, e.currentTarget.value)}/><br/>
-      <span></span><br/>
-      <label>Apellido: </label><br/>
-      <input type="text" 
-            name = "apellido"
-            placeholder="Apellido"
-            onChange={(e)=>handlePersona(e.currentTarget.name, e.currentTarget.value)}/><br/>
-      <span></span><br/>
-      <button>Registrar</button>
-    </form>
+    <>
+      <form>
+        <h1>{Persona.nombre} {Persona.apellido}</h1>
+        <label>Nombre: </label>
+        <input type="text"
+                placeholder="Nombre"
+                name = "nombre"
+                onChange={(e)=>handlePersona(e.currentTarget.name, e.currentTarget.value)}/>
+        <span></span><br />
+        <label>Apellido: </label>
+        <input type="text" 
+                placeholder="apellido"
+                name = "apellido"
+                onChange={(e)=>handlePersona(e.currentTarget.name, e.currentTarget.value)} /> 
+        <br />
+        <button onClick={()=>handleRegistrar}>Registrar</button>
+      </form>
+    </>
   );
 }
